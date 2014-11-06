b22e0d2fd749c073aec37fbd48a34b2f0b2bc77b
Donald Cook <Donald.Cook@rga.com>
2014-11-06 08:58:05 -0600

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
+    notify = require('gulp-notify'),
     connect = require('connect'),
     serveStatic = require('serve-static'),
     path = require('path'),
     connectLivereload = require('connect-livereload'),
     tinylr = require('tiny-lr'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
     sass = require('gulp-ruby-sass');
 
 // standard LiveReload port
@@ -20,10 +23,20 @@ gulp.task('sass', function() {
   gulp.src('app/scss/app.scss')
     // TODO: get sourcemaps working
     .pipe(sass({sourcemap: 'none', sourcemapPath: '/scss/'}))
-    .on('error', function(err) {
-      console.log(err.message);
-    })
+    .on('error', handleErrors)
     .pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('browserify', function() {
+  return browserify({
+    entries: ['./app/js/app.js'],
+    debug: true
+  })
+  .bundle()
+  .on('error', handleErrors)
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./dest/js/'))
+  .pipe(connect.reload());
 });
 
 gulp.task('lr', function() {
@@ -49,3 +62,14 @@ gulp.task('default', ['move', 'sass', 'lr', 'serve'], function() {
     tinylr.changed(path.relative(__dirname, event.path));
   });
 });
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+
+  this.emit('end');
+}
